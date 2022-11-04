679f915d20a03371e1b278c2dda946f6862f4014
BellaMalo <bmaldo23@gmail.com>
2022-11-04 15:43:50 -0700

    initial commit

---
+++ src/components/welcome.js
@@ -0,0 +1,15 @@
+function Welcome (){
+    
+
+    return(
+        <div className="Welcome-page-container">
+            <h1>Welcome</h1>
+            <button className="welcombtn">Nice to Meet You!</button>
+            <img className="cactus" src={require('./cactus-3.png')} alt='cactus'></img>
+
+        </div>
+    )
+
+}
+
+export default Welcome
