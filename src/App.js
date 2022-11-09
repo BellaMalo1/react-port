c16957fa2380ab8f00b2497c8b71ab332c7a4033
BellaMalo <bmaldo23@gmail.com>
2022-11-08 17:48:05 -0800

    header photo added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Hello from "./components/hello"
 import Welcome from './components/welcome';
 import Home from './components/home'
+import Header from './components/header';
 
 function App() {
   return (
     <div>
       {/* < Welcome /> */}
+      <Header/>
       <Home />
 
       
---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+function Header (){
+    return(
+        <>
+        <img className="welcome-image" src={require('./Screen Shot 2022-06-24 at 3.00.17 PM.jpg')}alt="Desert Scene"></img>
+        </>
+    )
+}
+export default Header
