493a2a13f0fbd5c523c92f87e40e4c55313efa2d
BellaMalo <bmaldo23@gmail.com>
2022-11-08 17:17:21 -0800

    home component added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from "./components/hello"
 import Welcome from './components/welcome';
+import Home from './components/home'
 
 function App() {
   return (
     <div>
       < Welcome />
+      
       
     </div>
   );
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,20 +1,30 @@
 import welcome from './welcome.css'
+import { Link } from 'react-router-dom'
 
 function Welcome (){
 
    function handleSubmit(event){
     event.preventDefault()
     document.body.classList.add('hide')
+   
+   
+    
+
+
    }
 
 
     return(
+        
         <div className="Welcome-page-container">
             <h1>Welcome</h1>
-            <button onClick={handleSubmit}assName="welcombtn">Nice to Meet You!</button>
+            <button onClick={handleSubmit}ClassName="welcombtn">Nice to Meet You!</button>
             <img className="cactus" src={require('./cactus-3.png')} alt='cactus'></img>
-
         </div>
+        
+               
+
+        
     )
 
 }
